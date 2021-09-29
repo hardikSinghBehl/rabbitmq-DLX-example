981a3704c6137f2e31f09937e36ede27cb4b5cd2
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-09-29 20:39:52 +0530

    initial project commit

---
+++ src/main/java/com/behl/brahma/RabbitmqDeadLetterQueueExampleApplication.java
@@ -0,0 +1,13 @@
+package com.behl.brahma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqDeadLetterQueueExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqDeadLetterQueueExampleApplication.class, args);
+	}
+
+}
